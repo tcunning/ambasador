7891574aadae0347c83c05b731e2448e79c3863f
Tod Cunningham <tcunning@fivelakesstudio.com>
2015-11-06 08:47:58 -0500

    Basic class based API handlers

--- referral/urls.py
+++ referral/urls.py
@@ -7,9 +7,9 @@ from referral import views
 # http://localhost:8000/referral/test2/
 # http://localhost:8000/referral
 urlpatterns = [
-    url(r'^referral/$', views.referral_list),
+    url(r'^referral/$', views.ReferralList.as_view()),
     #url(r'^referral/(?P<pk>[0-9]+)/$', views.referral_detail),
-    url(r'^referral/(?P<theName>[0-9A-Za-z%_.]+)/$', views.referral_detail),
+    url(r'^referral/(?P<theName>[0-9A-Za-z%_.]+)/$', views.ReferralDetail.as_view()),
 ]
 
 # Allow support for (.json or .api)
--- referral/views.py
+++ referral/views.py
@@ -1,52 +1,55 @@
 """
 http://www.django-rest-framework.org/tutorial/2-requests-and-responses/
 """
-
-from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from referral.models import Referral
 from referral.serializers import ReferralSerializer
 
 # http://localhost:8000/referral/
 #
-@api_view(['GET', 'POST'])
-def referral_list(request, format=None):
-	if request.method == 'GET':
-		referral = Referral.objects.all()
-		serializer = ReferralSerializer(referral, many=True)
-		return Response(serializer.data)
+class ReferralList(APIView):
+    def get(self, request, format=None):
+        referral = Referral.objects.all()
+        serializer = ReferralSerializer(referral, many=True)
+        return Response(serializer.data)
 
-	elif request.method == 'POST':
-		serializer = ReferralSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format=None):
+        serializer = ReferralSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 # http://localhost:8000/referral/test/
 #
-@api_view(['GET', 'PUT', 'DELETE'])
-def referral_detail(request, theName, format=None):
-	try:
-		referral = Referral.objects.get(name=theName)
-	except Referral.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+class ReferralDetail(APIView):
+    def get_object(self, theName):
+        try:
+            return Referral.objects.get(name=theName)
+        except Referral.DoesNotExist:
+            raise Http404
 
-	if request.method == 'GET':
-		serializer = ReferralSerializer(referral)
-		referral.incrementCountNow()
-		return Response(serializer.data)
+    def get(self, request, theName, format=None):
+        referral = self.get_object(theName)
+        serializer = ReferralSerializer(referral)
+        referral.incrementCountNow()
+        return Response(serializer.data)
 
-	elif request.method == 'PUT':
-		serializer = ReferralSerializer(referral, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, theName, format=None):
+        referral = self.get_object(theName)
+        serializer = ReferralSerializer(referral, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	elif request.method == 'DELETE':
-		referral.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, theName, format=None):
+        referral = self.get_object(theName)
+        referral.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
